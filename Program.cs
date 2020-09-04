4a79ec813f9a4bb7f93ffdec572b67703d131617
Marlon <marlonsmoreira@gmail.com>
2020-09-04 10:39:53 -0300

    Aula FileStream e StreamReader

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+
+namespace FileStream
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"c:\Temp\File1.txt";
+            StreamReader rs = null;
+            try
+            {
+
+                rs = File.OpenText(path);
+                while(!rs.EndOfStream)
+                {
+                    string line = rs.ReadLine();
+                    Console.WriteLine(line);
+                }
+            }
+
+            catch (IOException e)
+            {
+                Console.WriteLine("An error occurred");
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                if (rs != null)
+                    rs.Close();
+                
+            }
+        }
+    }
+}
